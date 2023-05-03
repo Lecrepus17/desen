343b7cf137f0cffd3f02c452dbdec9512238bda3
sidmaroli <119603972+sidmaroli@users.noreply.github.com>
2023-05-03 10:51:57 -0300

    ds

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const express = require("express")
 const bodyParser = require("body-parser")
 
-let cursos = [];
+let cursos = [{id: 1, nome: "info"}];
+let alunos = [{id: 1, nome: "nome1"}];
 let ID = 1;
 
 const port = process.env.PORT
@@ -14,31 +15,60 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static('public'))
 
-app.get('/sobre', (_,res) => {
-    res.json({nome: "Pedro"})
+
+
+
+app.get('/alunos', (req, res) =>{
+    res.json(alunos);
+});
+
+app.post('/alunos', (req,res) => {
+    const {nome} = req.body
+    alunos.push({id: ID++, nome})
+    res.json({message: 'OK'})
 })
-app.post('/cursos/alterar/:id', (req, res) => {
+
+
+
+
+
+/*app.get('/sobre', (_,res) => {
+    res.json({nome: "Pedro"})
+})*/
+
+/*app.post('/cursos/alterar/:id', (req, res) => {
     const {nome} = req.body
     const {id} = req.params
     res.json({nome})
-})
+})*/
 
 app.get('/cursos', (req,res) => {
     res.json(cursos)
 })
 
+// adiciona curso
 app.post('/cursos', (req,res) => {
     const {nome} = req.body
     cursos.push({id: ID++, nome})
     res.json({message: 'OK'})
 })
 
-app.listen(port, () => {
-    console.log(`servidor rodando na porta ${port}`)
-})
+// altera curso
+app.put('/cursos/:id', (req, res) => {
+    const id = req.params.id;
+    const cursoAtualizado = req.body;
+    res.send(`Curso com id ${id} atualizado com sucesso!`);
+  })
 
+// remove um curso
 app.delete('/cursos/:id', (req,res) => {
     const {id} = req.params
     cursos = cursos.filter(c => c.id != id)
-    res.json({message: "eeee"})
+    res.send(`Curso com id ${id} removido com sucesso!`);
+})
+
+
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`)
 })
