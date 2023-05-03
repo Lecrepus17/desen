06ae774e216a2e164856485f0b64d5d140b9ece0
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-05-03 11:10:34 -0300

    a

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const moment = require('moment');
     let cursos = [
       { id: 0, nome: 'Curso de Node.js' },
     ];
-    let aluno = [
+    let alunos = [
         { id: 0, nome: 'pedro', curso: 0, data_nasc:'2006/04/17' },
       ];
 const port = process.env.PORT
@@ -38,12 +38,56 @@ app.get('/cursos', (req,res) => {
     res.json(cursos)
 })
   
-
-
   // adiciona um curso
 app.post('/cursos', (req,res) => {
     const {nome} = req.body
     cursos.push({id: ID++, nome})
+    res.json({message: 'OK'})
+})
+  
+  // altera um curso
+  app.post('/cursos/alterar/:id', (req, res) => {
+    const { nome } = req.body;
+    const { id } = req.params;
+    cursos = cursos.map(curso => {
+      if (curso.id == id) {
+        curso.nome = nome;
+      }
+      return curso;
+    });
+    const curso = cursos.find(c => c.id == id);
+    res.json({nome})
+    res.send(`Curso ${curso.nome} atualizado com sucesso!`);
+})
+
+
+  // remove um curso
+  app.delete('/cursos/:id', (req,res) => {
+    const {id} = req.params
+    const curso = cursos.find(c => c.id == id);
+    cursos = cursos.filter(c => c.id != id)
+    res.send(`Curso ${curso.nome} removido com sucesso!`);
+})
+
+
+// lista todos os alunos
+app.get('/alunos', (req,res) => {
+    const alunosComNomeCurso = alunos.map(a => {
+        const curso = cursos.find(c => c.id === a.curso);
+        const nomeCurso = curso ? curso.nome : 'Curso não encontrado';
+        return { ...a, nomeCurso };
+      });
+      res.json(alunosComNomeCurso);
+    res.json(alunos)
+    
+})
+  
+  // adiciona um aluno
+app.post('/cursos', (req,res) => {
+    const {nome} = req.body
+    const {curso} = req.body
+    const {data_nasc} = req.body
+    cursos.push({id: ID++, nome, curso,data_nasc})
     res.json({message: 'OK'})
 })
   
