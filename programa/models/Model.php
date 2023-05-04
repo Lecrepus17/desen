9fdadf735b6287a7858912ae1d9955f03e4f9bb7
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-05-04 09:00:23 -0300

    a

--- programa/models/Model.php
+++ programa/models/Model.php
@@ -3,7 +3,7 @@
 class Model{
     private $driver = 'mysql';
     private $host = 'localhost';
-    private $dbname = 'sistematwig';
+    private $dbname = 'programa';
     private $port = '3306';
     private $user = 'root';
     private $password = null;
