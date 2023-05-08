811a29c8f4c9141daaa435557e6932e320339d7a
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-05-08 15:37:20 -0300

    lista o compartilhar e retirada de pasta indesejada

--- programa/documentos_part.php
+++ programa/documentos_part.php
@@ -8,19 +8,15 @@
     $com = new Compartilhamento();
     $doc = new Documento();
     $compartilhamentos = $com->getIdDoc(['idUserCom' => $_SESSION['user']]);
-var_dump($compartilhamentos);
 
-foreach ($compartilhamentos as $compartilhamento) {
-    echo $compartilhamento['idDocCom'] . "<br>";
-}
-    foreach ($compartilhamentos as $compartilhamento) {
-        $documentos = $doc->getAll(['iddocumentos' => $compartilhamento['idDocCom'], 'OR']);
-    }
+    $documentos = $doc->getALL();
+
+
     
     
-    
-    echo $twig->render('documentos_lista.html', [
+    echo $twig->render('documentos_part.html', [
         'doc' => $documentos,
+        'com' => $compartilhamentos
         ]);
 
     
