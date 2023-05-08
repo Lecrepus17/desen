075ec0d5b10b2765458384046c21d5097fefa14b
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-05-08 15:51:52 -0300

    final do dia

--- programa/documentos_part.php
+++ programa/documentos_part.php
@@ -5,14 +5,12 @@
     require('models/Compartilhamento.php');
     require('models/Usuario.php');
     require('models/Documento.php');
+
     $com = new Compartilhamento();
     $doc = new Documento();
     $compartilhamentos = $com->getIdDoc(['idUserCom' => $_SESSION['user']]);
-
     $documentos = $doc->getALL();
 
-
-    
     
     echo $twig->render('documentos_part.html', [
         'doc' => $documentos,
